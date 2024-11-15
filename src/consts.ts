b5a8c22f02019762f47fe512f25d7b102c1e256b
ChristalC8 <christalcaton@gmail.com>
2024-11-15 14:03:23 +1100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,6 @@
 export const SITE_TITLE = "Christal";
 export const SITE_DESCRIPTION =
   "Hi I'm Christal. Welcome to my site.";
+"I'm a Graphic Designer based in the Northern Rivers area."
+"I work with brandning, packaging and layout". 
+
