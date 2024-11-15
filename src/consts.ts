dd01d15c2dba7e1504cd15c330348533408ca83e
ChristalC8 <christalcaton@gmail.com>
2024-11-15 14:50:27 +1100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
 export const SITE_TITLE = "Christal";
 export const SITE_DESCRIPTION =
-"Hi I'm Christal."
-"I'm a Graphic Designer based in the Northern Rivers." 
+"Hi I'm Christal. I'm a Graphic Designer based in the Northern Rivers." 
 
