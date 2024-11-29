609168964244e7f385a0c4cb7318dfedfde1efcf
Christal <christalcaton@gmail.com>
2024-11-29 20:25:57 +1100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,30 +1,7 @@
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Christal Caton - Graphic Designer</title>
-<link rel="stylesheet" href="styles.css">
-</head>
-<body>
-<header>
-<h1>Transforming ideas into impactful designs.</h1>
-<p>Christal Caton, your go-to graphic designer in the Northern Rivers, NSW, for branding, print, and digital media.</p>
-</header>
+export const SITE_TITLE = "Christal";
+export const SITE_DESCRIPTION =
+"Hi I'm Christal. I'm a Graphic Designer based in the Northern Rivers." 
 
-<section id="latest-projects">
-<h2>Latest Projects:</h2>
-<div class="project">
-<img src="path/to/your/image.jpg" alt="Giga Premium Local Coffee Logo">
-<h3>GIGA</h3>
-<p>PREMIUM LOCAL COFFEE</p>
-</div>
-</section>
-
-<footer>
-<p>&copy; 2024 Christal Caton. All rights reserved.</p>
-</footer>
-</body>
-</html>
 
 
 
