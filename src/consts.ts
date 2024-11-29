b7877a36c9ac49df79bbee23f5c1347676f894eb
Christal <christalcaton@gmail.com>
2024-11-29 18:45:04 +1100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,6 @@
 export const SITE_TITLE = "Christal";
-export const SITE_DESCRIPTION = `
-Transforming ideas into impactful designs.<br />
-Christal Caton, your go-to graphic designer in the Northern Rivers, NSW, for branding, print, and digital media.
-`;
+export const SITE_DESCRIPTION = "Transforming ideas into impactful designs.\n\nChristal Caton, your go-to graphic designer in the Northern Rivers, NSW, for branding, print, and digital media.";
+
 
 
 
