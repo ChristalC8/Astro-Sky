30906b0697f9cf0b37b07ce419416dbcce773a2e
Christal <christalcaton@gmail.com>
2024-11-29 14:23:25 +1100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,12 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://astro-sky.vercel.app",
+  integrations: [mdx(), sitemap()],
+  build: {
+    assets: "assets",
+  },
+});
 
