015a570bf9d6ae1f8cb6d147612cd9daa0969599
Christal <christalcaton@gmail.com>
2024-11-29 18:57:54 +1100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "Christal";
-export const SITE_DESCRIPTION =
-"Transforming ideas into impactful designs".
+export const SITE_DESCRIPTION = "Transforming ideas into impactful designs"
+
 
 
 
