9ccf90ca0313fe57278d1c8704fa53a7b70aa484
Christal <christalcaton@gmail.com>
2024-11-30 15:08:12 +1100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,17 @@
 export const SITE_TITLE = "Christal";
 export const SITE_DESCRIPTION =
 "Transforming ideas into impactful designs."
----
-  export const SITE_TITLE = "Christal";
-export const SITE_DESCRIPTION =
-"Christal Caton, your go to graphic designer in the Northern Rivers, NSW, for branding, print, and digital media." 
 
----
+Hi, I'm Christal.
 
+<img src="/my-photo-1.jpg" alt="A photo of me" style={{ width: '207.87401574803147px', height: '207.87401574803147' }} />
 
+I am graphic designer with a passion for branding, layout design, and packaging. I enjoy combining illustrative and photographic elements with typography to craft designs that captivate and engage.
+
+I enjoy exploring creative ways to bring concepts to life, whether through clean, minimalistic layouts or dynamic abstract gradients.
+From designing compelling layouts to creating versatile advertising campaigns, my goal is to deliver designs that are both visually impactful and purpose-driven.
+
+To get in touch please direct message my via my [Instagram](https://www.instagram.com/christal_cu.8/), [LinkedIn](https://www.linkedin.com/in/christal-caton-73981818a/), or [Behance](https://www.linkedin.com/in/christal-caton-73981818a/) networks or email me directly at [christalcaton@gmail](mailto:christalcaton+website@gmail).
 
 
 
